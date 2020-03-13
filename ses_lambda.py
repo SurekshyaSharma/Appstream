8266283079e9118a8a8ac3ecc3bf0ddffc3f1475
SurekshyaSharma <43008704+SurekshyaSharma@users.noreply.github.com>
2020-03-13 11:54:50 -0500

    Create ses_lambda.py

---
+++ ses_lambda.py
@@ -0,0 +1,67 @@
+import boto3
+from botocore.exceptions import ClientError
+import json
+import os
+import time
+import uuid
+import decimal
+
+# https://aws.amazon.com/blogs/architecture/create-dynamic-contact-forms-for-s3-static-websites-using-aws-lambda-amazon-api-gateway-and-amazon-ses/
+
+var AWS = require('aws-sdk');
+var ses = new AWS.SES();
+ 
+var RECEIVER = 'contactus@example.com';
+var SENDER = 'contactus@example.com';
+
+var response = {
+ "isBase64Encoded": false,
+ "headers": { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'example.com'},
+ "statusCode": 200,
+ "body": "{\"result\": \"Success.\"}"
+ };
+
+exports.handler = function (event, context) {
+    console.log('Received event:', event);
+    sendEmail(event, function (err, data) {
+        context.done(err, null);
+    });
+};
+ 
+function sendEmail (event, done) {
+    var params = {
+        Destination: {
+            ToAddresses: [
+                RECEIVER
+            ]
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: 'name: ' + event.name + '\nphone: ' + event.phone + '\nemail: ' + event.email + '\ndesc: ' + event.desc,
+                    Charset: 'UTF-8'
+                }
+            },
+            Subject: {
+                Data: 'Website Referral Form: ' + event.name,
+                Charset: 'UTF-8'
+            }
+        },
+        Source: SENDER
+    };
+    ses.sendEmail(params, done);
+}
+
+
+# Add policy
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Sid": "VisualEditor0",
+#             "Effect": "Allow",
+#             "Action": "ses:SendEmail",
+#             "Resource": "*"
+#         }
+#     ]
+# }
